deb1382e1299b9deaee90b26879ba3b2f534c01b
Ionut Nitescu <nitescucornelionut19@gmail.com>
2023-03-14 21:45:46 +0200

    start implementation on user profile

--- apps/user/src/app/user-profile/user-profile.component.ts
+++ apps/user/src/app/user-profile/user-profile.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Users} from "../../../../../libs/shared/models/User/Users";
 
 @Component({
   selector: 'delivery-client-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss'],
 })
-export class UserProfileComponent {}
+export class UserProfileComponent implements OnInit{
+  user!:Users;
+  constructor(private httpClient:HttpClient) {
+  }
+  ngOnInit() {
+    this.httpClient.get<Users>('Accounts/current').subscribe((userData)=>{
+      this.user=userData;
+    })
+  }
+}
